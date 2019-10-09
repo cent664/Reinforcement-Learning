4091747166c4a7b8555fd9689a94eb0a969317c7
Agnibh Dasgupta <44358874+fatfat664@users.noreply.github.com>
2019-10-08 18:05:38 -0600

    Add files via upload

--- Old_DQN_Project/DQN.py
+++ Old_DQN_Project/DQN.py
@@ -267,14 +267,14 @@ def visualization():  # To visualize intermediate layers
     filters = (filters - f_min) / (f_max - f_min)
 
     # plot first few filters
-    n_filters, ix = 6, 1
+    n_filters, ix = 64, 1
     for i in range(n_filters):
         # get the filter
         f = filters[:, :, :, i]
         # plot each channel separately
-        for j in range(3):
+        for j in range(32):
             # specify subplot and turn of axis
-            ax = plt.subplot(n_filters, 3, ix)
+            ax = plt.subplot(n_filters, 32, ix)
             ax.set_xticks([])
             ax.set_yticks([])
             # plot filter channel in grayscale
