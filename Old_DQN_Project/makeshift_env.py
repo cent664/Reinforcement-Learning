ed1f9f5694ac5dfcd75e820c8438918d5ece24fe
Agnibh Dasgupta <44358874+fatfat664@users.noreply.github.com>
2019-11-08 13:19:03 -0700

    Add files via upload

--- Old_DQN_Project/makeshift_env.py
+++ Old_DQN_Project/makeshift_env.py
@@ -31,9 +31,10 @@ class StockTradingEnv:
         Adjusted Closing Price of the stock at the start of the time-series
     """
 
-    def __init__(self, mode, steps, stock, trend):
+    def __init__(self, mode, steps, stock, trend, window_size):
         self.stockname = stock
         self.trendname = trend
+        self.filename = self.stockname + "_" + self.trendname
 
         # Reading the data
         if mode == 'Test':
@@ -46,13 +47,20 @@ class StockTradingEnv:
         self.data_stocks_date = self.df_stocks['Date']
         self.data_stocks_date = pd.to_datetime(self.data_stocks_date)
         self.data_stocks_date = self.data_stocks_date.dt.date
+        self.data_trends_date = self.df_trends['Date']
+        self.data_trends_date = pd.to_datetime(self.data_trends_date)
+        self.data_trends_date = self.data_trends_date.dt.date
 
         self.data_stocks_open = self.df_stocks['Open'].values
         self.data_stocks_close = self.df_stocks['Close'].values
         self.data_stocks_low = self.df_stocks['Low'].values
         self.data_stocks_high = self.df_stocks['High'].values
+        self.data_trends_open = self.df_trends['Open'].values
+        self.data_trends_close = self.df_trends['Close'].values
+        self.data_trends_low = self.df_trends['Low'].values
+        self.data_trends_high = self.df_trends['High'].values
 
-        self.window_size = 16  # Number of data points in the state
+        self.window_size = window_size  # Number of data points in the state
         self.index = self.window_size - 1  # Initial state index (Add ints to shift the starting index)
         self.static_image_size = (self.window_size, self.window_size)  # Shape of the input image into the CNN
 
@@ -60,13 +68,22 @@ class StockTradingEnv:
         self.observation_space = (1, self.window_size, self.window_size)
 
         # Calculating Scaling factor
-        maxRange = -1000000
+        stocks_maxRange = -1000000
         for i in range(0, steps):
-            maxRange = max(maxRange, max(self.data_stocks_high[i:i + self.window_size]) - min(self.data_stocks_low[i:i + self.window_size]))
+            stocks_maxRange = max(stocks_maxRange, max(self.data_stocks_high[i:i + self.window_size]) - min(self.data_stocks_low[i:i + self.window_size]))
 
-        self.dollars_per_pixel = maxRange/self.window_size
-        self.scaling_factor = 1 / self.dollars_per_pixel
-        self.scaling_factor = self.scaling_factor / 2  # To account for the shift from centering close
+        self.stocks_dollars_per_pixel = stocks_maxRange/self.window_size
+        self.stocks_scaling_factor = 1 / self.stocks_dollars_per_pixel
+        self.stocks_scaling_factor = self.stocks_scaling_factor / 2  # To account for the shift from centering close
+
+        # Calculating Scaling factor
+        trends_maxRange = -1000000
+        for i in range(0, steps):
+            trends_maxRange = max(trends_maxRange, max(self.data_trends_high[i:i + self.window_size]) - min(self.data_trends_low[i:i + self.window_size]))
+
+        self.trends_dollars_per_pixel = trends_maxRange / self.window_size
+        self.trends_scaling_factor = 1 / self.trends_dollars_per_pixel
+        self.trends_scaling_factor = self.trends_scaling_factor / 2  # To account for the shift from centering close
 
         # Calculating the date range of the data in question (used when saving graphs/files)
         self.start_stocks_date = self.df_stocks['Date'][self.window_size]
@@ -76,20 +93,23 @@ class StockTradingEnv:
 
     def compute_im(self, current_price_index, window_size):
 
+        # Stock image
         test_array = compute_array(self.df_stocks, current_price_index, window_size)
-        test_array = reduce_dim(test_array, self.scaling_factor)
-        im_data = coloring(test_array, self.static_image_size)
+        test_array = reduce_dim(test_array, self.stocks_scaling_factor)
+        stocks_im_data = coloring(test_array, self.static_image_size)
 
-        """Uncomment this code out to test Manu's images"""
-        # test = np.zeros([64, 16])
-        #
-        # test = np.expand_dims(test, axis=0)
+        # Trend image
+        test_array = compute_array(self.df_trends, current_price_index, window_size)
+        test_array = reduce_dim(test_array, self.trends_scaling_factor)
+        trends_im_data = coloring(test_array, self.static_image_size)
+
+        trends_im_data = np.expand_dims(trends_im_data, axis=0)
+        stocks_im_data = np.expand_dims(stocks_im_data, axis=0)
+
+        im_data = np.concatenate([stocks_im_data, trends_im_data], axis=0)
+
         # im_data = np.expand_dims(im_data, axis=0)
-        #
-        # im_data = np.concatenate([im_data, test], axis=0)
-
-        im_data = np.expand_dims(im_data, axis=0)
-        im_data = np.expand_dims(im_data, axis=0)
+        # im_data = np.expand_dims(im_data, axis=0)
 
         return im_data
 
@@ -100,7 +120,7 @@ class StockTradingEnv:
         im = self.compute_im(self.index, self.window_size)
         self.state = im
 
-        return self.state, self.date_stocks_range, self.stockname
+        return self.state, self.date_stocks_range, self.filename
 
     def step(self, action, mode):
 
