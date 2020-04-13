ef86b4b13aaaa0177ba65958afb8791c62f65486
Agnibh Dasgupta <44358874+fatfat664@users.noreply.github.com>
2020-04-13 03:04:43 -0600

    Add files via upload

--- Old_DQN_Project/acceptable_dims.py
+++ Old_DQN_Project/acceptable_dims.py
@@ -12,9 +12,20 @@ def inputlayerdim(w_out, f, p, s):
 def magic():
     for w_out in range(1, 20):
         w_1 = inputlayerdim(w_out, 3, 0, 1)
-        w_2 = inputlayerdim(w_1, 4, 0, 2)
-        w_3 = inputlayerdim(w_2, 8, 0, 4)
+        w_2 = inputlayerdim(w_1, 4, 0, 1)
+        w_3 = inputlayerdim(w_2, 8, 0, 1)
         print(w_3)
 
+
+def outputlayerdim(w_in, f, p, s):
+    w_out = 1 + (w_in - f + (2 * p))/s
+    return w_out
+
 if __name__ == '__main__':
-    magic()
+    # magic()
+    w_out = outputlayerdim(16, 8, 0, 1)
+    print(w_out)
+    w_out = outputlayerdim(w_out, 4, 0, 1)
+    print(w_out)
+    w_out = outputlayerdim(w_out, 3, 0, 1)
+    print(w_out)
